366626eaaad93349512aa0e7fc3246c3dc954679
russeva3@gmail.com <Elizarusev15>
2017-12-23 15:04:55 +0000

    Content Uploaded

--- 04. JavaScript - Syntax Basic Lab/2.Calculate expression.js
+++
@@ -1,5 +0,0 @@
-function calcExpression() {
-    let result = (30 + 25) *( 1/3) * (35 - 14 - 12) * (30 + 25) *( 1/3) * (35 - 14 - 12);
-    console.log(result)
-}
-calcExpression()
--- 04. JavaScript - Syntax Basic Lab/3.Sum Two Numbers.js
+++
@@ -1,7 +0,0 @@
-function sumTwoNumbers(nums) {
-    let num1 = Number(nums[0]);
-    let num2 = Number(nums[1]);
-    let result = num1 + num2;
-    console.log(result)
-}
-sumTwoNumbers(['10', '20']);
